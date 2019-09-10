7f68d02174850f91cdb3e7c86748b5ac2cedb868
Damir Valeev <Damir_Valeev@epam.com>
2019-09-10 21:23:55 +0400

    Add pages&css

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -9,6 +9,6 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("d342432").forward(request, response);
     }
 }
